3c100cfdb5c391d49c7e8f19bba9bd9aab1999d8
Kla Haeck <kla@zsh.email>
2022-08-22 10:22:16 -0500

    first commit

---
+++ contract-438e54ca00.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable@4.7.2/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable@4.7.2/utils/CountersUpgradeable.sol";
+
+/// @custom:security-contact kla@zsh.email
+contract Gargoyle is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC721BurnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    string public baseURI = "https://gargoyles-ui-development.herokuapp.com/gargoyles/api/meta/";
+
+    // mapping(uint256 => address) internal _tokenIdToAddress;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC721_init("Gargoyle", "GARG");
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
+        __Pausable_init();
+        __AccessControl_init();
+        __ERC721Burnable_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory uri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        baseURI = uri;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory uri) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setTokenURI(tokenId, uri);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        // _tokenIdToAddress[tokenId] = to;
+    }
+
+    function getTokenIdsByOwner(address _owner) public view returns (uint256[] memory) {
+        uint256[] memory _tokensOfOwner = new uint256[](ERC721Upgradeable.balanceOf(_owner));
+        for (uint256 i = 0; i < ERC721Upgradeable.balanceOf(_owner); i++) {
+            _tokensOfOwner[i] = ERC721EnumerableUpgradeable.tokenOfOwnerByIndex(_owner, i);
+        }
+        return (_tokensOfOwner);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+        // _tokenIdToAddress[tokenId] = to;
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+    {
+        super._burn(tokenId);
+        // delete _tokenIdToAddress[tokenId];
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
